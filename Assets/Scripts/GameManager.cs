0267629cf91b4d62e4158b5ace53d390f6dd1b73
DanielBergshoeff <dabergshoeff@gmail.com>
2018-09-13 12:34:43 +0200

    Added some functionality and prefabs.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static Text text;
+
+    public enum GameState {
+        PREGAME,
+        INGAME,
+        POSTGAME
+    }
+
+    public static GameState currentState;
+
+    private static int points;
+
+    public static int Points {
+        get {
+            return points;
+        }
+        set {
+            if(text == null) {
+                text = GameObject.Find("txtPoints").GetComponent<Text>();
+            }
+
+            points = value;
+            text.text = "Points: " + points.ToString();
+        }
+    }
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/SphereGaze.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GazeAware))]
+public class SphereGaze : MonoBehaviour {
+
+    private float timeBeforeSwitch;
+
+    public float TimeBeforeSwitch {
+        get {
+            return timeBeforeSwitch;
+        }
+        set {
+            if(timeBeforeSwitch > 0 && value <=  0) {
+                GetComponent<Renderer>().material.color = colorAfterSwitch;
+                if (gainPoints) {
+                    GameManager.Points += 10;
+                }
+                else {
+                    GameManager.Points -= 10;
+                }
+            }
+            timeBeforeSwitch = value;
+        }
+    }
+
+    public int initTimeBeforeSwitch;
+
+    public bool gainPoints;
+
+    public Color colorBeforeSwitch;
+    public Color colorAfterSwitch;
+
+    public Text timeBeforeSwitchText;
+
+    private GazeAware _gazeAware;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = colorBeforeSwitch;
+        _gazeAware = GetComponent<GazeAware>();
+        TimeBeforeSwitch = initTimeBeforeSwitch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_gazeAware.HasGazeFocus)
+        {
+            if (TimeBeforeSwitch > 0f)
+                TimeBeforeSwitch -= Time.deltaTime;
+        }
+
+        if(TimeBeforeSwitch <= 0)
+        {
+            TimeBeforeSwitch = 0f;
+        }
+
+        timeBeforeSwitchText.text = timeBeforeSwitch.ToString();
+	}
+}
